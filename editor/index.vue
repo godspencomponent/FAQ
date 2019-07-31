de4395ce7badeadf6e6f4986d39d53cf2342b573
liulei <lei.liul@ymm56.com>
2019-07-31 18:49:22 +0800

    对话式组件

---
+++ editor/index.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="component-editor">
+    <div>
+      <span>样式：</span>
+      <div class='list-type' @click='chooseType(1)'>
+        <img src="https://ymm-maliang.oss-cn-hangzhou.aliyuncs.com/ymm-maliang/resource/ymm_1564566114092.png" alt="文字列表" />
+        <div class="tip">对话式FAQ</div>
+        <i class="el-icon-circle-check" v-if='componentInfo.type == 1'></i>
+      </div>
+      <div class='list-type' @click='chooseType(2)'>
+        <img src="https://ymm-maliang.oss-cn-hangzhou.aliyuncs.com/ymm-maliang/resource/ymm_1564569032193.png" alt="文字列表" />
+        <div class="tip">普通FAQ</div>
+        <i class="el-icon-circle-check" v-if='componentInfo.type == 2'></i>
+      </div>
+    </div>
+    <el-card class="box-card" header='基础配置'>
+      <div slot="header" class="clearfix">
+        <span>列表项</span>
+        <el-button style="float: right; padding: 3px 0" type="text" @click='addItem'>新增列表项</el-button>
+      </div>
+      <div>
+        <el-collapse accordion>
+          <el-collapse-item :name="i" v-for='(v, i) in componentInfo.list' :key='i'>
+            <template slot="title">
+              {{i + 1 + '.' + v.name}}<i class="el-icon-delete el-collapse-item__arrow del-item" @click.stop='delItem(i)'></i>
+            </template>
+            <el-form ref="form" label-width="80px" label-position='right' size='mini'>
+              <el-form-item label="设为提问者:" label-width="100px" v-if='componentInfo.type == 1'>
+                <el-switch v-model="v.isSender" active-color="#1890ff" inactive-color="#bbbbbb"> </el-switch>
+              </el-form-item>
+              <el-form-item label="头像:">
+                <attr-resource type="image" :url.sync="v.img"></attr-resource>
+              </el-form-item>
+              <el-form-item label="标题:" v-if='componentInfo.type == 2'>
+                <div class="tag">
+                  <el-input v-model="v.name" placeholder="请输入标题"></el-input>
+                  <el-color-picker v-model="v.titleColor" show-alpha></el-color-picker>
+                </div>
+              </el-form-item>
+              <el-form-item label="标题字号:" v-if='componentInfo.type == 2'>
+                    <el-input placeholder="请输入文字大小" v-model="v.titleFont" min='0' type='number'>
+                        <template slot="append">px</template>
+                      </el-input>
+                  </el-form-item>
+              <el-form-item label="昵称:" v-if='componentInfo.type == 1'>
+                <el-input v-model="v.nick" placeholder="请输入昵称"></el-input>
+              </el-form-item>
+              <el-form-item label="内容:">
+                <attr-richtext :id="i" v-model='v.content'></attr-richtext>
+              </el-form-item>
+            </el-form>
+          </el-collapse-item>
+        </el-collapse>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'maliangeditor',
+    props: {
+      componentInfo: { // 固定字段，收集所有属性值
+        type: [Object],
+        default () {
+          return {
+            type: 1,
+            list: []
+          }
+        }
+      }
+    },
+    data: function () {
+      return {
+      }
+    },
+    computed: {
+    },
+    watch: {
+      componentInfo: {
+        hanlder: function (v) {
+          console.log('editor index', v)
+        },
+      }
+    },
+    mounted: function () {
+    },
+    methods: {
+      chooseType (type) {
+        this.componentInfo.type = type
+        if (type == 1) {
+          this.componentInfo.list = [
+            {
+              name: '问',
+              isSender: true,
+              img: 'https://ymm-maliang.oss-cn-hangzhou.aliyuncs.com/ymm-maliang/resource/ymm_1564561942960.jpg',
+              nick: '靓女',
+              content: '码良有哪些能力？'
+            },
+            {
+              name: '答',
+              isSender: false,
+              img: 'https://ymm-maliang.oss-cn-hangzhou.aliyuncs.com/ymm-maliang/resource/ymm_1564561415790.jpeg',
+              nick: '帅磊',
+              content: '可以快速制作H5页面。无需掌握复杂的编程技术，通过简单拖拽、少量配置即可制作精美的页面，可用于营销场景下的页面制作。'
+            }
+          ]
+        } else if (type == 2) {
+          this.componentInfo.list = [{
+            name: '什么是码良？',
+            titleColor: '#000',
+            titleFont: 16,
+            img: 'https://ymm-maliang.oss-cn-hangzhou.aliyuncs.com/ymm-maliang/resource/ymm_1564568927763.png',
+            content: '可以快速制作H5页面。无需掌握复杂的编程技术，通过简单拖拽、少量配置即可制作精美的页面，可用于营销场景下的页面制作。'
+          }]
+        }
+      },
+      delItem (i) {
+        this.componentInfo.list.splice(i, 1)
+      },
+      addItem () {
+        let newArr = this.componentInfo.list.concat(this.componentInfo.list)
+        this.componentInfo.list = JSON.parse(JSON.stringify(newArr))
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
+  .component-editor {
+    .list-type{
+      margin 10px 0 0 30px
+      max-width 250px
+      position relative
+      border 1px solid #505152
+      padding 10px
+      border-radius 5px
+      img{
+        width 100%
+      }
+      i {
+        position absolute
+        right 10px
+        bottom 15px
+        font-size 22px
+        color #409EFF
+      }
+      .tip{
+        display: none
+        width 100%
+        position: absolute
+        background-color: rgba(0, 0, 0, 0.7)
+        text-align center
+        font-size 12px
+        padding 3px 0
+        bottom 0
+        left 0
+        color #ccc
+      }
+      &:hover{
+        .tip{
+          display: block
+        }
+      }
+    }
+    .box-card {
+      margin-top 30px
+      .tag{
+        padding-right 80px
+        position relative
+        margin-bottom 5px
+        .el-color-picker{
+          position absolute
+          right 30px
+          top 0
+        }
+        i {
+          display none
+          position absolute
+          right 0
+          top 5px
+          color #F56C6C
+          cursor: pointer
+        }
+        &:hover {
+          i {
+            display block
+          }
+        }
+      }
+    }
+  }
+</style>
---
+++ src/index.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="component">
+    <div class="list">
+      <template>
+        <div v-if='type == 1'>
+          <div class="faq" :class="[v.isSender ? 'is-sender' : 'not-sender']" v-for='(v, i) in list' :key='i'>
+            <div class="faq-content" v-if='v.isSender'>
+              <div v-html='v.content'></div>
+            </div>
+            <div class="faq-user">
+              <img :src="v.img" alt="">
+              <span class="faq-user-nick">{{v.nick}}</span>
+            </div>
+            <div class="faq-content" v-if='!v.isSender'>
+              <div v-html='v.content'></div>
+            </div>
+          </div>
+        </div>
+        <div v-else>
+          <div class="faq2" v-for='(v, i) in list' :key='i'>
+            <section>
+              <div class="headPortrait">
+                <img :src="v.img" alt="">
+              </div>
+              <div class="faq2-content">
+                <h1 class="fd-title" :style="{color: v.titleColor, fontSize: v.titleFont}">{{v.name}}</h1>
+                <div class="faq2-body">
+                  <div v-html='v.content'></div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </template> 
+    </div>
+  </div>
+</template>
+
+<script>
+  import {VueExtend} from 'godspen-lib'
+
+  export default {
+    mixins: [VueExtend.mixin],
+    name: 'FAQ',
+    label: process.env.LABEL,
+    style: process.env.STYLE,
+    props: {
+      type: {
+        type: Number,
+        default: 1,
+        editor: {
+          ignore: true
+        }
+      },
+      list: {
+        type: Array,
+        default () {
+          return [
+            {
+              name: '1.问',
+              isSender: true,
+              img: 'https://ymm-maliang.oss-cn-hangzhou.aliyuncs.com/ymm-maliang/resource/ymm_1564561942960.jpg',
+              nick: '靓女',
+              content: '码良有哪些能力？'
+            },
+            {
+              name: '2.答',
+              isSender: false,
+              img: 'https://ymm-maliang.oss-cn-hangzhou.aliyuncs.com/ymm-maliang/resource/ymm_1564561415790.jpeg',
+              nick: '帅磊',
+              content: '可以快速制作H5页面。无需掌握复杂的编程技术，通过简单拖拽、少量配置即可制作精美的页面，可用于营销场景下的页面制作。'
+            }
+          ]
+        },
+      }
+    },
+    computed: {
+    },
+    editorMethods: {
+    },
+    methods: {
+      delItem (i) {
+        this.componentInfo.list.splice(i, 1)
+      },
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
+  .component {
+    padding-top 20px
+    width 100%
+    .faq{
+      display: flex;
+      min-width: 60px;
+      padding: 8px 12px;
+      background-color: transparent;
+    }
+    .faq2 {
+      padding 8px 15px 8px 12px
+      section {
+        position relative
+        .headPortrait {
+          position: absolute;
+          top: 0;
+          left: 0;
+          img {
+            width 30px
+            height 30px
+          }
+        }
+        .faq2-content {
+          padding-left 40px
+          .fd-title {
+            font-size: 16px;
+            color: #333;
+          }
+          .faq2-body {
+            margin-top: 10px;
+            font-size: 14px;
+            color: #333;
+          }
+        }
+      }
+    }
+    .is-sender {
+      justify-content: flex-end;
+      .faq-content {
+        color: #fff;
+        border: 1px solid #4947bb;
+        background-color: #6260e1;
+        &:before {
+          content ''
+          right: -5.5px;
+          background: #6260e1;
+          border: 1px solid #4947bb;
+          border-bottom: 0;
+          border-left: 0;
+        }
+      }
+      .faq-user {
+        margin-left 12px
+      }
+    }
+    .not-sender  {
+      justify-content: start;
+      .faq-user {
+        margin-right 12px
+      }
+      .faq-content {
+        border: 1px solid #d9d9d9;
+        color: #333;
+        background-color: #fff;
+        &:before {
+          content ''
+          left: -5.5px;
+          background: #fff;
+          border: 1px solid #d9d9d9;
+          border-top: 0;
+          border-right: 0;
+        }
+      }
+    }
+    .faq-content {
+      position: relative;
+      max-width: 220px;
+      height: fit-content;
+      padding: 8px 10px;
+      box-sizing: border-box;
+      border-radius: 8px;
+      font-size: 14px;
+      &:before {
+        content ''
+        position: absolute;
+        top: 14px;
+        width: 10px;
+        height: 10px;
+        -webkit-transform: rotate(45deg);
+        -ms-transform: rotate(45deg);
+        transform: rotate(45deg);
+        box-sizing: border-box;
+      }
+    }
+    .faq-user {
+      display: flex;
+      -webkit-box-orient: vertical;
+      -webkit-box-direction: normal;
+      flex-direction: column;
+      -webkit-box-align: center;
+      align-items: center;
+      width: 48px;
+      height: 65px;
+      img {
+        width: 40px;
+        height: 40px;
+        margin-bottom: 8px;
+        border-radius: 50%;
+        box-sizing: border-box;
+      }
+      .faq-user-nick {
+        width:48px;
+        text-align: center;
+        font-size: 12px;
+        color: #999;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+  }
+</style>
